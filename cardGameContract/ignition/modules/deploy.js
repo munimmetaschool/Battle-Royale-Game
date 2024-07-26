ee93d7d16efd7463c78a7fd0b5caac444d136a0c
MunimIftikhar <129931419+munimmetaschool@users.noreply.github.com>
2024-07-26 18:18:18 +0500

    Add CoreNFT

--- cardGameContract/ignition/modules/deploy.js
+++ cardGameContract/ignition/modules/deploy.js
@@ -7,8 +7,8 @@ async function main() {
     "TradingCardGame"
   );
 
-  const cardContractAddress = "0xB90A3E75e6128ab85cbae3985B348dbab7144a37"; // Replace with your actual contract address
-  const computerAddress = "0xb5B908F8214f122f34ACe62528D8d0AF8BD96828"; // Replace with the computer's address (can be the deployer's address)
+  const cardContractAddress = "0x238d43c23CFc5d5dE4d691fFd881492E14Bcfa4A"; // Replace with your actual contract address
+  const computerAddress = "0x0f82b919Fa748fbCFA82cdd970550e09cc9aaBe2"; // Replace with the computer's address (can be the deployer's address)
 
   const tradingCardGame = await TradingCardGame.deploy(
     cardContractAddress,
